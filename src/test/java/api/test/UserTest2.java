7090995cae65e251c8201bc666459797f92e6934
Lagna <lagna.work@gmail.com>
2024-06-15 13:01:28 +0200

    this is my first restapi commit

---
+++ src/test/java/api/test/UserTest2.java
@@ -0,0 +1,77 @@
+package api.test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints2;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTest2 {
+	Faker faker;
+	User userPayload;
+	public Logger logger; // for logs
+	@BeforeClass
+	public void setupData() {
+		faker=new Faker();
+		userPayload=new User();
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().emailAddress());
+		userPayload.setPassword(faker.internet().password());
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		//logs
+				logger= LogManager.getLogger(this.getClass());
+				
+				logger.debug("debugging.....");
+	}
+	@Test(priority=1)
+	public void testPostUser() {
+		logger.info("********** Creating user  ***************");
+		Response response=UserEndPoints2.createUser(userPayload);
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("**********User is created  ***************");
+	}
+	@Test(priority=2)
+	public void testGetUserByName() {
+		logger.info("********** Reading User Info ***************");
+		Response response=UserEndPoints2.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("**********User info  is displayed ***************");
+	}
+	
+	@Test(priority=3)
+	public void testUpdateUser() {
+		logger.info("********** Updating User ***************");
+		//update data using payload
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().emailAddress());
+		Response response=UserEndPoints2.updateUser(this.userPayload.getUsername(), userPayload);//passing the updated payload here
+		response.then().log().body();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********** User updated ***************");
+		//checking response after update
+		Response responseAfterUpdate=UserEndPoints2.readUser(this.userPayload.getUsername());
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
+		
+	}
+	@Test(priority=4)
+	public void testDeleteUserByName() {
+		logger.info("**********   Deleting User  ***************");
+		Response response=UserEndPoints2.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********** User deleted ***************");
+	}
+
+
+
+}
